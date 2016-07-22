da6dd229594adcb0b2da2e57eb008dfae6e79641
Jennifer Cen <cenjennifer@gmail.com>
2016-07-22 00:12:23 -0400

    moving key to production

--- server/env/production.js
+++ server/env/production.js
@@ -9,19 +9,19 @@
 module.exports = {
     "DATABASE_URI": "postgres://gyeqnrrwoeqxvq:TBUuGuxuTUfNLLbWEnt6wEN30y@ec2-107-20-136-222.compute-1.amazonaws.com:5432/dtdqtnu33knt5",
     "SESSION_SECRET": "Essence729",
-    "TWITTER": {
-        "consumerKey": process.env.TWITTER_CONSUMER_KEY,
-        "consumerSecret": process.env.TWITTER_CONSUMER_SECRET,
-        "callbackUrl": process.env.TWITTER_CALLBACK
+       "TWITTER": {
+        "consumerKey": "t6Ue52sQt5kTH4EgOqF0k9Y1K",
+        "consumerSecret": "u5UcVVano15nzdTYoGOejgbzgAlR6MoNW1YxDIQUvrCEmmUoCx",
+        "callbackUrl": "http://127.0.0.1:1337/auth/twitter/callback"
     },
     "FACEBOOK": {
-        "clientID": process.env.FACEBOOK_APP_ID,
-        "clientSecret": process.env.FACEBOOK_CLIENT_SECRET,
-        "callbackURL": process.env.FACEBOOK_CALLBACK_URL
+        "clientID": "260739110983828",
+        "clientSecret": "f2e4baf1a94846f0d0ff58cc3e0406dd",
+        "callbackURL": "http://127.0.0.1:1337/auth/facebook/callback"
     },
     "GOOGLE": {
-        "clientID": process.env.GOOGLE_CLIENT_ID,
-        "clientSecret": process.env.GOOGLE_CLIENT_SECRET,
-        "callbackURL": process.env.GOOGLE_CALLBACK_URL
+        "clientID": "301555922169-1v7anfcc7kbhai5d60bdahq94dbbbp2v.apps.googleusercontent.com",
+        "clientSecret": "tftozyr7LXA-1MHYVXKKWB67",
+        "callbackURL": "http://127.0.0.1:1337/auth/google/callback"
     }
 };
