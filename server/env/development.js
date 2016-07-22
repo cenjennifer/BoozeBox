42d6e6a10587056e68049517ae50071d25549db4
Jennifer Cen <cenjennifer@gmail.com>
2016-07-22 00:08:11 -0400

    updating dev env

--- server/env/development.js
+++ server/env/development.js
@@ -1,19 +1,21 @@
 module.exports = {
-  "DATABASE_URI": "postgres://localhost:5432/fsg",
-  "SESSION_SECRET": "Optimus Prime is my real dad",
-  "TWITTER": {
-    "consumerKey": "t6Ue52sQt5kTH4EgOqF0k9Y1K",
-    "consumerSecret": "u5UcVVano15nzdTYoGOejgbzgAlR6MoNW1YxDIQUvrCEmmUoCx",
-    "callbackUrl": "http://127.0.0.1:1337/auth/twitter/callback"
-  },
-  "FACEBOOK": {
-    "clientID": "260739110983828",
-    "clientSecret": "f2e4baf1a94846f0d0ff58cc3e0406dd",
-    "callbackURL": "http://127.0.0.1:1337/auth/facebook/callback"
-  },
-  "GOOGLE": {
-    "clientID": "301555922169-1v7anfcc7kbhai5d60bdahq94dbbbp2v.apps.googleusercontent.com",
-    "clientSecret": "tftozyr7LXA-1MHYVXKKWB67",
-    "callbackURL": "http://127.0.0.1:1337/auth/google/callback"
-  }
+    // "DATABASE_URI": "postgres://localhost:5432/fsg",
+    // "SESSION_SECRET": "Optimus Prime is my real dad",
+    "DATABASE_URI": "postgres://gyeqnrrwoeqxvq:TBUuGuxuTUfNLLbWEnt6wEN30y@ec2-107-20-136-222.compute-1.amazonaws.com:5432/dtdqtnu33knt5",
+    "SESSION_SECRET": "Essence729",
+    "TWITTER": {
+        "consumerKey": "t6Ue52sQt5kTH4EgOqF0k9Y1K",
+        "consumerSecret": "u5UcVVano15nzdTYoGOejgbzgAlR6MoNW1YxDIQUvrCEmmUoCx",
+        "callbackUrl": "http://127.0.0.1:1337/auth/twitter/callback"
+    },
+    "FACEBOOK": {
+        "clientID": "260739110983828",
+        "clientSecret": "f2e4baf1a94846f0d0ff58cc3e0406dd",
+        "callbackURL": "http://127.0.0.1:1337/auth/facebook/callback"
+    },
+    "GOOGLE": {
+        "clientID": "301555922169-1v7anfcc7kbhai5d60bdahq94dbbbp2v.apps.googleusercontent.com",
+        "clientSecret": "tftozyr7LXA-1MHYVXKKWB67",
+        "callbackURL": "http://127.0.0.1:1337/auth/google/callback"
+    }
 };
